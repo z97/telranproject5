23c225c6a0aa0f97e2eb0c8862fe63b55595327e
kliba <klibaner@mail.com>
2021-12-25 21:20:04 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from "./components/list_event/Header/Header";
 import SmallEventsList from "./components/list_event/SmallEventsList/SmallEventsList";
