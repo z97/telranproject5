9151bdabda8b09d4602835dbbfa3d9ef1f989ddf
kliba <klibaner@mail.com>
2021-12-25 21:47:11 +0200

    3

--- src/components/SmallEvent.js
+++ src/components/SmallEvent.js
@@ -3,7 +3,13 @@ import React from 'react';
 const SmallEvent = (props) => {
     return (
         <div>
-            
+            <strong>{props.event.title}</strong>
+            <div>
+                {props.event.body}
+            </div>
+            <div>
+                <button onClick={() => router.push(`/`)}
+            </div>
         </div>
     );
 };
