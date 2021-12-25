5d87bacf801a408843310bdb73d1016db2b2e6ee
kliba <klibaner@mail.com>
2021-12-25 21:29:26 +0200

    3

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {Button} from "react-bootstrap";
 import ButtonPay from "./UI/buttons/ButtonPay";
 
-const EventForm = (props) => {
+const EventForm = ({create}) => {
     const [application, setApplication] = useState({name: '', cellphone: '', email: '', quantity: ''})
     const addNewApplication = (e) => {
         e.preventDefault()
@@ -14,11 +14,13 @@ const EventForm = (props) => {
     }
     return (
         <div>
-            <input type="text" placeholder="name" value={application.name} onChange={e => setApplication({...application, name: e.target.value})}/>
-            <input type="text" placeholder="cellnumber"{...props}/>
-            <input type="text" placeholder="email"{...props}/>
+            <div className="d-flex flex-column mb-3">
+            <h4><input type="text" value={application.name} onChange={e => setApplication({...application, name: e.target.value})} name="Имя" placeholder="Ваше имя..."/></h4>
+            <input type="text" name="telephone" placeholder="Ваш телефон" {...props}/>
+            <input type="text" placeholder="email" name="email"{...props}/>
             <input type="number" id="quantity" name="quantity" min="1" max="10"{...props}/>
             <ButtonPay onClick={addNewApplication}/>
+            </div>
         </div>
     );
 };
