65733c55c44cdd7d6b2a7ca3ace4729f76eb8fdb
Max <voronin223@gmail.com>
2022-01-08 14:53:02 +0200

    pravka

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/list_event/Header/Header";
 import SmallEventsList from "./components/list_event/SmallEventsList/SmallEventsList";
 import Index from "./components/list_event";
 import EventForm from "./components/EventForm";
+import AboutSvetoslav from "./components/page/AboutSvetoslav";
 
 function App() {
     return (
         <div>
-            <Index/>
-            <SmallEventsList/>
+            {/*<Index/>
+            <SmallEventsList/>*/}
+            <AboutSvetoslav/>
         </div>
     );
 }
